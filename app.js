22e7d1b257bf12b3627fec1afb3c56c1dbd55c3e
Anurag-singh-RBU <anuragsingh260805@gmail.com>
2025-07-21 23:40:58 +0530

    updated css

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ const {LIST_SCHEMA} = require("./schema.js");
 const {REVIEW_SCHEMA} = require("./schema.js");
 const Review = require("./models/review.js");
 const session = require("express-session");
+const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -42,10 +46,25 @@ const sessionOptions = {
     secret: "SECRET_KEY",
     resave: false,
     saveUninitialized: true,
+    cookie: {
+
+        expires : Date.now() + 7 * 24 * 60 * 60 * 1000, // 7 days (in ms) -> days * hrs * mins * secs * ms
+        maxAge : 7 * 24 * 60 * 60 * 1000,
+        httpOnly : true,
+
+    }
 
 }
 
 app.use(session(sessionOptions));
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/" , (req , res) => {
 
---
+++ models/user.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const plm = require("passport-local-mongoose");
+
+const USER_SCHEMA = new Schema({
+
+    email: {
+
+        type: String,
+        required: true,
+
+    },
+
+});
+
+User.plugin(plm);
+module.exports = mongoose.model("User", USER_SCHEMA);
